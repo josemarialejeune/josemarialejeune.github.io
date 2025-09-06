45b662e5bc94712286ec9218216eefe0544f4e25
José María Lejeune <89063849+josemarialejeune@users.noreply.github.com>
2025-09-06 23:35:03 +0200

    Ajuste de module/indes.js para operar con cms.js

--- assets/js/module/index.js
+++ assets/js/module/index.js
@@ -1,4 +1,4 @@
-import { setPage, setScript } from 'https://jsystemmicroservices.com/assets/module/cms.js';
+import { setPage, setScript } from 'https://jsystemmicroservices.com/js/assets/module/cms.js';
 /** Quitamos el spin */
 setScript("jsystemManagerSpinDeactivate");
 /** Carga de la pagina de inicio */
