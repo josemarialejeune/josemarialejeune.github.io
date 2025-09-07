8db7c405d3895926accfc930c505ce9272133e91
José María Lejeune <89063849+josemarialejeune@users.noreply.github.com>
2025-09-07 16:32:00 +0200

    Reajuste signin/index.js

--- assets/js/signin/index.js
+++ assets/js/signin/index.js
@@ -8,12 +8,6 @@ const jSystemManager = 'jSystemManager';
 signInWithPopup(auth, provider).then((result) => {
     const credential = GoogleAuthProvider.credentialFromResult(result);
     /**
-     * Carga en la sesion del explorador
-     */
-    window.app.session.init(jSystemManager);
-    window.app.session.set('credential', JSON.stringify(credential), jSystemManager);
-    window.app.session.set('user', JSON.stringify(result.user), jSystemManager);
-    /**
      * Ajustes de la pantalla
      *  Inicializamos el documento html  - setScript
      *  Cargamos la pagina jsystemManagerClient - setPage
@@ -21,16 +15,24 @@ signInWithPopup(auth, provider).then((result) => {
     setScript("jsystemManagerPortalInitialize");
     setPage("jsystemManagerPortal");
     /**
-     *  Procesamos la solicitud de autenticacion de Google
-     */
-    usrSet();
-    accessSet();
-    /**
      *  Usuario autenticado 
      *  Imagen y nombre del usuario en el sidebar
      */
     window.app.nodeLoad('app-main-nav-avatar-image').src=result.user.photoURL;
     window.app.nodeLoad('app-main-nav-avatar-name').innerHTML= result.user.displayName;
+    /**
+     * Carga en la sesion del explorador
+     */
+    window.app.session.init(jSystemManager);
+    window.app.session.set('credential', JSON.stringify(credential), jSystemManager);
+    window.app.session.set('user', JSON.stringify(result.user), jSystemManager);
+    /**
+     *  Procesamos la solicitud de autenticacion de Google
+     
+    usrSet();
+    accessSet();
+    */
+    
 }).catch((error) => {
     window.app.session.init(jSystemManager);
     window.app.session.set('error', JSON.stringify(error), jSystemManager);
